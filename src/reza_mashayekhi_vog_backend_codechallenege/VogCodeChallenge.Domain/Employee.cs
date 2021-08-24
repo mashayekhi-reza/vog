1529a0669c4de5bbe4ad440cb95bbb6b62d646bd
rezam62 <rezamashayekhi62@gmail.com>
2021-08-24 21:34:48 +0800

    Employee model added

---
+++ src/reza_mashayekhi_vog_backend_codechallenege/VogCodeChallenge.Domain/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace VogCodeChallenge.Domain
+{
+    public class Employee
+    {
+        internal Employee(string firstName, string lastName, string jobTitle, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            JobTitle = jobTitle;
+            Address = address;
+        }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string JobTitle { get; }
+        public string Address { get; }
+    }
+}
