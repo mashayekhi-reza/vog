cb73007e4c9ac24b7be8daad06bf173f3204eae3
rezam62 <89461977+rezam62@users.noreply.github.com>
2021-08-25 22:02:54 +0800

    question4 (#4)
    * Employee controller added
    
    * swagger added

---
+++ src/reza_mashayekhi_vog_backend_codechallenege/VogCodeChallenge.API/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VogCodeChallenge.ApplicationServices;
+using VogCodeChallenge.Domain;
+
+namespace VogCodeChallenge.API.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _employeeService.GetAll();
+        }
+
+        [HttpGet("department/{departmentId}")]
+        public IEnumerable<Employee> Get(Guid departmentId)
+        {
+            return _employeeService.GetByDepartment(departmentId);
+        }        
+    }
+}
--- src/reza_mashayekhi_vog_backend_codechallenege/VogCodeChallenge.API/Startup.cs
+++ src/reza_mashayekhi_vog_backend_codechallenege/VogCodeChallenge.API/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VogCodeChallenge.ApplicationServices;
 
 namespace VogCodeChallenge.API
 {
@@ -22,15 +16,22 @@ namespace VogCodeChallenge.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IEmployeeService, EmployeeService>();
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(s=> 
+            {
+                s.SwaggerEndpoint("swagger/v1/swagger.json", "Vog API V1");
+                s.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
