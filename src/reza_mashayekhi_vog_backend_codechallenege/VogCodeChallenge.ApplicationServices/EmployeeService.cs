6899ab040208c35582525775528e415bd5852f84
rezam62 <89461977+rezam62@users.noreply.github.com>
2021-08-25 21:32:31 +0800

    question3

---
+++ src/reza_mashayekhi_vog_backend_codechallenege/VogCodeChallenge.ApplicationServices/EmployeeService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VogCodeChallenge.Domain;
+
+namespace VogCodeChallenge.ApplicationServices
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly Company _company;
+
+        public EmployeeService()
+        {
+            _company = new Company("Vog");
+            _company.AddDepartment("IT", "Here!");
+            var it = _company.Departments.Single();
+            it.AddEmployee("John", "Smith", "IT man1", "address IT 1");
+            it.AddEmployee("Peter", "White", "IT man2", "address IT 2");
+
+            _company.AddDepartment("Business", "There!");
+            var business = _company.Departments.Last();
+            business.AddEmployee("John", "Doe", "Business Man1", "address business 1");
+            business.AddEmployee("Jean", "Doe", "Business Woman2", "address business 2");
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _company.Departments.SelectMany(d => d.Employees);
+        }
+
+        public IList<Employee> ListAll()
+        {
+            return GetAll().ToList();
+
+        }
+
+    }
+}
