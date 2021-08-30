a080c47be7b880ac9933c115233397606bddb79c
rezam62 <89461977+rezam62@users.noreply.github.com>
2021-08-30 15:34:52 +0800

    TESTModule added

--- src/VogCodeChallenge.ConsoleApp/QuestionClass.cs
+++ src/VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using VogCodeChallenge.Shared.Exceptions;
 
 public static class QuestionClass
 {
@@ -8,4 +9,17 @@ public static class QuestionClass
         "Jeffrey",
         "John",
     };
+
+    public static dynamic TESTModule(dynamic input)
+    {
+        return input switch
+        {
+            int n when (n > 0 && n < 5) => input * 2,
+            int n when (n > 4) => input * 3,
+            int n when (n < 1) => throw new InvalidArgumentException("The input cannot be less than 1"),
+            float n when (n == 1.0f || n == 2.0f) => 3.0f,
+            string n => n.ToUpper(),
+            _ => input,
+        };
+    }
 }
