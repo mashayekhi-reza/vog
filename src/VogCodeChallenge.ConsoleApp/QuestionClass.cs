c38150ce492c09676be5c6ee611dfcf8bee9b74d
rezam62 <89461977+rezam62@users.noreply.github.com>
2021-08-30 13:30:22 +0800

    console application added

---
+++ src/VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public static class QuestionClass
+{
+    public static List<string> NamesList = new List<string>()
+    {
+        "Jimmy",
+        "Jeffrey",
+        "John",
+    };
+}
