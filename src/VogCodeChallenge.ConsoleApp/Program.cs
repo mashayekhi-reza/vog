c97d8a94e8a660d12989d6b948134d4e9a6209f0
rezam62 <rezamashayekhi62@gmail.com>
2021-08-30 13:29:54 +0800

    console application added

---
+++ src/VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.NamesList.Select(n => { Console.WriteLine(n); return n; }).ToList();
+        }
+    }
+}
