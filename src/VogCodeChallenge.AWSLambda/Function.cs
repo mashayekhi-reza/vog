ba7e78611377e8a5d54c10da9752a22e44969505
rezam62 <rezamashayekhi62@gmail.com>
2021-08-31 12:35:27 +0800

    AWS Lambda Project added

---
+++ src/VogCodeChallenge.AWSLambda/Function.cs
@@ -0,0 +1,25 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace VogCodeChallenge.AWSLambda
+{
+    public class Function
+    {
+        public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+            foreach (var record in dynamoEvent.Records)
+            {
+                context.Logger.LogLine($"Event ID: {record.EventID}");
+                context.Logger.LogLine($"Event Name: {record.EventName}");
+                context.Logger.LogLine($"Event Source: {record.EventSource}");
+            }
+
+            context.Logger.LogLine("Stream processing complete.");
+        }
+    }
+}
